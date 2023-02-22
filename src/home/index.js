04e2d0c04d649038743b2727b9476cf1dce95b77
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-22 11:05:03 -0300

    feat: add properties on map

--- src/home/index.js
+++ src/home/index.js
@@ -1,37 +1,84 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Dimensions,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 
 import * as Location from 'expo-location';
-import MapView from 'react-native-maps';
-import {styles} from './styles';
+import MapView, {Callout} from 'react-native-maps';
+import {styles, mapStyle} from './styles';
 
 export default function Home() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const {width, height} = Dimensions.get('window');
+  const ASPECT_RATIO = width / height;
+  const LATITUDE_DELTA = 0.02;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
+  const [errorMsg, setErrorMsg] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      let {status} = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+  async function getPermition() {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    getCoords();
+    if (status !== 'granted') {
+      setErrorMsg(
+        'Permission to access location was denied, please go to settings and allow Take a Bus to know your location.',
+      );
+    }
   }
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-      <Text style={styles.text}>{text}</Text>
-    </View>
-  );
+
+  async function getCoords() {
+    let {coords} = await Location.getCurrentPositionAsync({});
+    setLocation({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    });
+  }
+  useEffect(() => {
+    getPermition();
+  }, [location.latitude]);
+
+  if (location.latitude != 0) {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            initialRegion={location}
+            OnMapready={getPermition()}
+            showsPointsOfInterest={true}
+            showsUserLocation={true}
+            loadingEnabled={true}
+            customMapStyle={mapStyle}
+            userLocationUpdateInterval={3000}
+            zoomEnabled={true}
+            zoomTapEnabled={true}
+            zoomControlEnabled={true}
+          />
+          <Callout style={styles.buttonCallout}>
+            <TouchableOpacity
+              style={[styles.touchable]}
+              onPress={() => console.log('press')}>
+              <Text style={styles.touchableText}>Bus Stops</Text>
+            </TouchableOpacity>
+          </Callout>
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.permissonDenied}>{errorMsg}</Text>
+      </View>
+    );
+  }
 }
