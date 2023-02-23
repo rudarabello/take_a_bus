facae85d88446233bfbd2ea276cca3282e878f6a
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-23 01:16:38 -0300

    feat: add env and config files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
+/* eslint-disable no-undef */
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['module:react-native-dotenv'],
   };
 };
