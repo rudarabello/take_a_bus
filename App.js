bba70c0aec3657825337b0497ab335cc15e500d1
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-24 15:55:12 -0300

    style: change name of file

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {DataProvider} from './src/contexts/provider.js';
-import Home from './src/pages/home/index.js';
+import Home from './src/pages/home/home.js';
 
 export default function App() {
   return (
