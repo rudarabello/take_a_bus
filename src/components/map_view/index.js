c98f5182fbf6d6c853ed2a1feff82d0fb6d7d729
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-24 14:37:11 -0300

    feat: add map_view as component

--- src/components/map_view/index.js
+++ src/components/map_view/index.js
@@ -2,32 +2,36 @@ import React from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import {styles, mapStyle} from './styles';
 
+// eslint-disable-next-line react/prop-types
 export const Map = ({location, visiblePoints, busStops, setButton, button}) => {
-  <MapView
-    style={styles.map}
-    initialRegion={location}
-    showsPointsOfInterest={true}
-    showsUserLocation={true}
-    loadingEnabled={true}
-    customMapStyle={mapStyle}
-    userLocationUpdateInterval={3000}
-    zoomEnabled={true}
-    zoomTapEnabled={true}
-    onMapReady={() => setButton(!button)}
-    zoomControlEnabled={true}>
-    {visiblePoints &&
-      busStops.map(e => {
-        return e.map((places, index) => {
-          return (
-            <Marker
-              key={index}
-              coordinate={{
-                latitude: places.place.location.lat,
-                longitude: places.place.location.lng,
-              }}
-            />
-          );
-        });
-      })}
-  </MapView>;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={location}
+      showsPointsOfInterest={true}
+      showsUserLocation={true}
+      loadingEnabled={true}
+      customMapStyle={mapStyle}
+      userLocationUpdateInterval={3000}
+      zoomEnabled={true}
+      zoomTapEnabled={true}
+      onMapLoaded={() => setButton(!button)}
+      zoomControlEnabled={true}>
+      {visiblePoints &&
+        // eslint-disable-next-line react/prop-types
+        busStops.map(e => {
+          return e.map((places, index) => {
+            return (
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: places.place.location.lat,
+                  longitude: places.place.location.lng,
+                }}
+              />
+            );
+          });
+        })}
+    </MapView>
+  );
 };
